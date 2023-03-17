8a0da9b27914aac25aa97bbc5b17a9817850f2f0
vmarshirov <vmarshirov@outlook.com>
2023-03-17 22:03:58 +0300

    e...

--- orm_store_app/admin.py
+++ orm_store_app/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from .models import  Category, Item
 
-# Register your models here.
-# @admin.register(Store)
-# class StoreAdmin(admin.ModelAdmin):
-#     pass
-#     list_display = ['pk', 'task', 'a', 'b', 'c', 'current_date']
-#     list_editable = ['task', 'a', 'b', 'c']
-#     search_fields = ['task', 'a', 'b', 'c']
-#     list_filter = ['task', 'a', 'b', 'c']
-#     list_per_page = 5
-
 admin.site.register(Category)
 admin.site.register(Item)
+
+# Register your models here.
+# @admin.register(Item)
+# class ItemAdmin(admin.ModelAdmin):
+#     pass
+#     list_display = ['id', 'category', 'name',  'price']
+#     list_editable = ['name', 'category']
+#     search_fields = ['name']
+#     list_filter = ['name', 'price']
+#     list_per_page = 15
